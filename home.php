29795af92f789d069a0c92bf9428cedbfcc9f180
j9Tuan <cabahugjanine140@gmail.com>
2016-09-27 14:24:03 +0800

    ayawg ka hurt

--- home.php
+++ home.php
@@ -3,6 +3,6 @@
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 </head>
 <body>
-	<button class="btn btn-danger">Beaches are Red. Yes.</button>
+	<button class="btn btn-danger"><i class='glyphicon glyphicon-remove'></i>Beaches are Red. Yes.</button>
 </body>
 </html>
